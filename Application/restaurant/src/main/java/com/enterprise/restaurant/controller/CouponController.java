5fc078708fbd373465d4a4a98c10e82059c0c100
乔嘉伟 <qiaojiawei0612@gmail.com>
2020-06-26 21:31:26 -0400

    coupon and restaurant database built user need some fix

--- Application/restaurant/src/main/java/com/enterprise/restaurant/controller/CouponController.java
+++ Application/restaurant/src/main/java/com/enterprise/restaurant/controller/CouponController.java
@@ -1,5 +1,6 @@
-package com.enterprise.restaurant.dataobject;
+package com.enterprise.restaurant.controller;
 
+import com.enterprise.restaurant.dataobject.Coupon;
 import com.enterprise.restaurant.repository.CouponRepository;
 import java.math.BigDecimal;
 import java.util.List;
@@ -7,10 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/coupon")
 public class CouponController {
   @Autowired
   private CouponRepository repository;
