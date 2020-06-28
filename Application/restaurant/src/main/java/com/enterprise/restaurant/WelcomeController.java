0acf8aacc229851fa1b2e677ad629ce35692532d
乔嘉伟 <qiaojiawei0612@gmail.com>
2020-06-27 21:50:52 -0400

    fixed some typos

--- Application/restaurant/src/main/java/com/enterprise/restaurant/WelcomeController.java
+++ Application/restaurant/src/main/java/com/enterprise/restaurant/WelcomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
-  @GetMapping("welcome")
+  @GetMapping("/welcome")
   public String say(){
     return "Welcome Page!";
   }
