d9440702ef58ed7883aa7056d151cbe02871c4bd
乔嘉伟 <qiaojiawei0612@gmail.com>
2020-06-30 23:51:37 -0400

    Fixed two bugs

--- demo3/app.py
+++ demo3/app.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, request, redirect, url_for, session
 from exts import db
 from models import User, Coupon
 import config
+import hashlib
 import os
 
 app = Flask(__name__)
@@ -18,30 +19,58 @@ def index():
     return render_template('home.html')
 
 
-@app.route('/', methods = ['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    errmsg = []
     if request.method == 'GET':
         return render_template('login.html')
+    elif 'user_id' in session:
+        return redirect(url_for('home'))
     else:
         email=request.form.get('email')
         password=request.form.get('password')
-        user = User.query.filter(User.email==email, User.password_hash ==password).first()
+        user = User.query.filter(User.email==email, User.password_hash==password).first()
         if user:
             session['user_id'] = user.id
             session.permanent = True
             return render_template("home.html")
         else:
-            return "incorrect email or password!"
+            errmsg.append("incorrect email or password")
+            return render_template("login.html", errmsg = errmsg)
+
+
+# @app.route('/', methods = ['GET', 'POST'])
+# @app.route('/login.html', methods = ['GET', 'POST'])
+# def login():
+#     if request.method == 'POST':
+#         # builds a dictinary with the input taken from the login fields
+#         validationInfo = {
+#             'email': request.form['email'],
+#             'password': (hashlib.md5(request.form['password'].encode())).hexdigest()
+#         }
+#         # Builds a user from login credentials
+#         user = buildUser(validationInfo)
+#         # creates a session with the given information
+#         if "uid" in user:
+#             session['account'] = user['uid']
+#             return render_template("home.html")
+#
+#     # User is already signed up, redirects them to the home page
+#     elif 'account' in session:
+#         return redirect(url_for('home'))
+#
+#     # Info is invalid or nothing has been submitted
+#     return render_template("login.html")
 
 
 # The home landing pages
 @app.route('/home.html')
 def home():
-    if 'account' in session:
+    if 'user_id' in session:
         return render_template('home.html')
     else:
-        return render_template('login.html')
+        return redirect(url_for('login'))
 
 
 # The registration options page
@@ -60,19 +89,21 @@ def user_register():
         password2 = request.form.get('password2')
         address = request.form.get('address')
         name = request.form.get('name')
-
-        # check if email already exist
-        user = User.query.filter(User.email == email).first()
-        if user:
-            errmsg.append("Email has already been used.")
+        if not (email or password or password2 or address or name):
+            errmsg.append("Please fill all required information")
         else:
-            if password==password2:
-                user = User(name=name, email=email, password_hash=password, address=address, exp=1, user_type=-1)
-                db.session.add(user)
-                db.session.commit()
-                return render_template('login.html')
+            # check if email already exist
+            user = User.query.filter(User.email == email).first()
+            if user:
+                errmsg.append("Email has already been used.")
             else:
-                errmsg.append("Passwords do not match.")
+                if password==password2:
+                    user = User(name=name, email=email, password_hash=password, address=address, exp=1, user_type=-1)
+                    db.session.add(user)
+                    db.session.commit()
+                    return redirect(url_for('login'))
+                else:
+                    errmsg.append("Passwords do not match.")
 
     return render_template("registration0.html", errmsg=errmsg)
 
@@ -107,21 +138,23 @@ def owner_register():
         password2 = request.form.get('password2')
         address = request.form.get('address')
         name = request.form.get('name')
-        rname = request.form.get('rname')
+        # rname = request.form.get('rname')
         # write more
-
-        # check if email already exist
-        user = User.query.filter(User.email == email).first()
-        if user:
-            errmsg.append("Email has already been used.")
+        if not (email or password or password2 or address or name):
+            errmsg.append("Please fill all required information")
         else:
-            if password == password2:
-                user = User(name=name, email=email, password_hash=password, address=address, exp=-1, user_type=1)
-                db.session.add(user)
-                db.session.commit()
-                return render_template('login.html')
+            # check if email already exist
+            user = User.query.filter(User.email == email).first()
+            if user:
+                errmsg.append("Email has already been used.")
             else:
-                errmsg.append("Passwords do not match.")
+                if password == password2:
+                    user = User(name=name, email=email, password_hash=password, address=address, exp=-1, user_type=1)
+                    db.session.add(user)
+                    db.session.commit()
+                    return redirect(url_for('login'))
+                else:
+                    errmsg.append("Passwords do not match.")
 
     return render_template("registration1.html", errmsg=errmsg)
 
@@ -132,11 +165,14 @@ def owner_register():
 def coupon():
     # coupons = getCouponsById({'uid': session['account']})
     # return render_template("coupon.html", owner = coupons != [], coupons = coupons)
+    # We need a user_coupon column
+
     return render_template("coupon.html")
 
 
 @app.route('/createCoupon.html', methods=['GET', 'POST'])
 def create_coupon():
+    errmsg = []
     if request.method == 'GET':
         return render_template('createCoupon.html')
     else:
@@ -144,11 +180,14 @@ def create_coupon():
         discount = request.form.get('discount')
         description = request.form.get('description')
         date = request.form.get('date')
+        if not (name or discount or date):
+            errmsg.append("Missing required information")
+        else:
+            coupon = Coupon(name=name, amount=discount, description=description, date=date)
+            db.session.add(coupon)
+            db.session.commit()
 
-        coupon = Coupon(name=name, amount=discount, description=description, date=date)
-        db.session.add(coupon)
-        db.session.commit()
-        return render_template("createCoupon.html")
+    return redirect(url_for('coupon'))
 
 
 # To end session you must logout
@@ -160,3 +199,4 @@ def logout():
 
 if __name__ == '__main__':
     app.run()
+
