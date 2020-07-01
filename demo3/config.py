3b9cacf4256edc63703dfe49187976e31e0e167b
nessa <vanessa242pierre@gmail.com>
2020-07-01 16:29:26 -0400

    Bug fixes

--- demo3/config.py
+++ demo3/config.py
@@ -4,17 +4,18 @@ DEBUG = True
 
 SECRET_KEY = os.urandom(24)
 
+# Prod
+
+# SQLALCHEMY_DATABASE_URI = "postgres://acakkhlblzjgdn:380973a230519b862a2e3b44923629e394f5ab8db48a1225b940030cd2df853f@ec2-54-161-208-31.compute-1.amazonaws.com:5432/d5skqebio5o5p9"
+# SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+
+# Test
 HOSTNAME = '127.0.0.1'
 PORT = '3306'
 DATABASE = 'demo_05'
 USERNAME = 'root'
 PASSWORD = '12345678'
 DB_URI = 'mysql+mysqldb://{}:{}@{}:{}/{}?charset=utf8'.format(USERNAME, PASSWORD, HOSTNAME, PORT, DATABASE)
-
 SQLALCHEMY_DATABASE_URI = DB_URI
 SQLALCHEMY_TRACK_MODIFICATIONS = True
-
-
-
-
-
