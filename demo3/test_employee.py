a664fb30b2ffc3996125ecb4cb6e756dc4c10fb1
yh-su <yolanda.su@mail.utoronto.ca>
2020-07-12 17:59:45 -0400

    added 2 new employee tests and refactored existing employee creation tests

--- demo3/test_employee.py
+++ demo3/test_employee.py
@@ -1,6 +1,7 @@
 from flask import Flask, session
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.exc import IntegrityError
+from sqlalchemy.orm.exc import FlushError
 from app import app
 from exts import db
 from models import User
@@ -30,55 +31,40 @@ class EmployeeDBTest(unittest.TestCase):
         pass
 
     def test_add_default_employee(self):
-        user = User(name='name', email='email', password='123', type=0)
-        restaurant = Restaurant(name='name', address='address')
         with app.app_context():
-            db.session.add(user)
-            db.session.add(restaurant)
+            user = self.add_user('name', 'email', '123', 0)
+            restaurant = self.add_restaurant('name', 'address')
+            employee = self.link_employee(user.uid, restaurant.rid)
             db.session.commit()
             self.assertIsNotNone(User.query.filter_by(email='email').first())
-
-            employee = Employee(uid = user.uid, rid = restaurant.rid)
-            db.session.add(employee)
-            db.session.commit()
             self.assertIsNotNone(Employee.query.filter_by(uid=user.uid).first())
 
-
     def test_add_employee_with_existing_employee_of_same_name(self):
-        user = User(name='name', email='email', password='123', type=0)
-        user2 = User(name='name', email='email2', password='123', type=0)
         with app.app_context():
-            db.session.add(user)
-            db.session.add(user2)
+            user = self.add_user('name', 'email', '123', 0)
+            user2 = self.add_user('name', 'email2', '123', 0)
             db.session.commit()
             self.assertIsNotNone(User.query.filter_by(email='email2').first())
     
     def test_add_employee_with_existing_customer_of_same_name(self):
-        user = User(name='name', email='email', password='123', type=-1)
-        user2 = User(name='name', email='email2', password='123', type=0)
         with app.app_context():
-            db.session.add(user)
-            db.session.add(user2)
+            user = self.add_user('name', 'email', '123', -1)
+            user2 = self.add_user('name', 'email2', '123', 0)
             db.session.commit()
             self.assertIsNotNone(User.query.filter_by(email='email2').first())
 
     def test_add_employee_with_existing_owner_of_same_name(self):
-        user = User(name='name', email='email', password='123', type=1)
-        user2 = User(name='name', email='email2', password='123', type=0)
         with app.app_context():
-            db.session.add(user)
-            db.session.add(user2)
+            user = self.add_user('name', 'email', '123', 1)
+            user2 = self.add_user('name', 'email2', '123', 0)
             db.session.commit()
             self.assertIsNotNone(User.query.filter_by(email='email2').first())
 
     def test_add_employee_with_existing_employee_of_same_email(self):
-        user = User(name='name', email='email', password='123', type=0)
-        user2 = User(name='name2', email='email', password='123', type=0)
         with app.app_context():
-            db.session.add(user)
+            user = self.add_user('name','email', '123', 0)
             db.session.commit()
-            db.session.add(user2)
-            
+            user2 = self.add_user('name2', 'email', '123', 0)
             with self.assertRaises(IntegrityError):
                 try:
                     db.session.commit()
@@ -88,13 +74,10 @@ class EmployeeDBTest(unittest.TestCase):
             self.assertEqual(1, User.query.filter_by(email='email').count())
     
     def test_add_employee_with_existing_customer_of_same_email(self):
-        user = User(name='name', email='email', password='123', type=-1)
-        user2 = User(name='name2', email='email', password='123', type=0)
         with app.app_context():
-            db.session.add(user)
+            user = self.add_user('name','email', '123', -1)
             db.session.commit()
-            db.session.add(user2)
-            
+            user2 = self.add_user('name2', 'email', '123', 0)
             with self.assertRaises(IntegrityError):
                 try:
                     db.session.commit()
@@ -104,13 +87,10 @@ class EmployeeDBTest(unittest.TestCase):
             self.assertEqual(1, User.query.filter_by(email='email').count())
 
     def test_add_employee_with_existing_owner_of_same_email(self):
-        user = User(name='name', email='email', password='123', type=1)
-        user2 = User(name='name2', email='email', password='123', type=0)
         with app.app_context():
-            db.session.add(user)
+            user = self.add_user('name','email', '123', 1)
             db.session.commit()
-            db.session.add(user2)
-            
+            user2 = self.add_user('name2', 'email', '123', 0)
             with self.assertRaises(IntegrityError):
                 try:
                     db.session.commit()
@@ -118,6 +98,52 @@ class EmployeeDBTest(unittest.TestCase):
                     db.session.rollback()
                     raise
             self.assertEqual(1, User.query.filter_by(email='email').count())
+
+    def test_link_multiple_employees_to_restaurant(self):
+        with app.app_context():
+            user = self.add_user('name', 'email', '123', 0)
+            user2 = self.add_user('name2', 'email2', '123', 0)
+            restaurant = self.add_restaurant('name', 'address')
+            db.session.commit()
+            
+            employee = self.link_employee(user.uid, restaurant.rid)
+            employee2 = self.link_employee(user2.uid, restaurant.rid)
+            db.session.commit()
+
+            self.assertEqual(2, Employee.query.filter_by(rid=restaurant.rid).count())
+
+    def test_link_employee_to_multiple_restaurants(self):
+        with app.app_context():
+            user = self.add_user('name', 'email', '123', 0)
+            restaurant = self.add_restaurant('name', 'address')
+            restaurant2 = self.add_restaurant('name2', 'address2')
+            employee = self.link_employee(user.uid, restaurant.rid)
+            db.session.commit()
+
+            employee2 = self.link_employee(user.uid, restaurant2.rid)
+            with self.assertRaises(FlushError):
+                try:
+                    db.session.commit()
+                except:
+                    db.session.rollback()
+                    raise
+            self.assertEqual(1, Employee.query.filter_by(uid=user.uid).count())
+
+    # Helper functions
+    def add_user(self, name, email, password, type):
+        user = User(name=name, email=email, password=password, type=type)
+        db.session.add(user)
+        return user
+
+    def add_restaurant(self, name, address):
+        restaurant = Restaurant(name=name, address=address)
+        db.session.add(restaurant)
+        return restaurant
+
+    def link_employee(self, uid, rid):
+        employee = Employee(uid = uid, rid = rid)
+        db.session.add(employee)
+        return employee
 
     if __name__ == "__main__":
         unittest.main()
