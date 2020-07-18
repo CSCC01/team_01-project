49e3346488d124047202abe43cd46bae64ed3ab1
Anson Feng <anson.feng@mail.utoronto.ca>
2020-07-18 18:27:24 -0400

    Added import to app.py

--- demo3/app.py
+++ demo3/app.py
@@ -5,6 +5,7 @@ from helpers.user import *
 from helpers.restaurant import *
 from helpers.employee import *
 from helpers.coupon import *
+from helpers.achievement import *
 import config
 import os
 import hashlib
