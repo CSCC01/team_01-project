b5494c159e246786395f43832dca970b030a1e2a
nessa <vanessa242pierre@gmail.com>
2020-07-24 17:44:10 -0400

    Removed get_customer_coupons_by_rid from coupon helper methods

--- demo3/helpers/coupon.py
+++ demo3/helpers/coupon.py
@@ -1,4 +1,4 @@
-from models import Coupon, User
+from models import Coupon
 
 
 import config
@@ -98,34 +98,3 @@ def delete_coupon(cid):
     coupon.deleted = 1
     db.session.commit()
     return None
-
-
-def get_customer_coupons_by_rid(rid):
-    """
-    Fetches rows from the customer_coupons, user and coupon table.
-
-    Retrieves all coupons that a specific restaurant has in circularion.
-
-    Args:
-        rid: A restaurant ID that corresponds to a restaurant in the
-          Customer_Coupons table. A integer.
-
-    Returns:
-        A list of dictinaries that contains information about each coupon a
-        customer has possession of at a specific resturant.
-    """
-    coupons = Customer_Coupons.query.filter(Customer_Coupons.rid == rid).all()
-    customer_coupon_list = []
-    for c in coupons:
-        user = User.query.filter(User.rid == c.uid).first()
-        coupon = Coupon.query.filter(Coupon.cid == c.cid).first()
-        dict = {
-            "email": user.email,
-            "cname": coupon.name,
-            "begin": coupon.begin,
-            "expiration": coupon.expiration,
-            "description": coupon.description,
-            "points": coupon.points
-        }
-        customer_coupon_list.append(dict)
-    return customer_coupon_list
