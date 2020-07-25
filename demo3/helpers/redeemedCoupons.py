0c779ee5cf7e63b7f0b635ea52e58c052577e688
nessa <vanessa242pierre@gmail.com>
2020-07-25 14:03:09 -0400

    T105: Added redeemCoupon, coupon and restaurant helpers to retrieve coupons from db

--- demo3/helpers/redeemedCoupons.py
+++ demo3/helpers/redeemedCoupons.py
@@ -1,5 +1,6 @@
 from models import Redeemed_Coupons
 from helpers.coupon import *
+from helpers.restaurant import *
 
 import config
 if config.STATUS == "TEST":
@@ -18,3 +19,17 @@ def get_redeemed_coupons_by_rid(rid):
         c['used'] = used
 
     return coupons
+
+
+def get_redeemed_coupons_by_uid(uid):
+    coupons = Redeemed_Coupons.query.filter(Redeemed_Coupons.uid == uid, Redeemed_Coupons.valid == 1).all()
+    coupon_list = []
+
+    for c in coupons:
+        dict = get_coupon_by_cid(c.cid)
+
+        dict["rname"] = get_restaurant_name_by_rid(c.rid)
+        dict["raddress"] = get_restaurant_address(c.rid)
+        coupon_list.append(dict)
+
+    return coupon_list
