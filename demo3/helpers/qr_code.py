6ae11307b16ecdfd6a1f1e2989a77eb7ea4b4202
乔嘉伟 <qiaojiawei0612@gmail.com>
2020-07-27 23:47:03 -0400

    Modified qr_code path to fit remote

--- demo3/helpers/qr_code.py
+++ demo3/helpers/qr_code.py
@@ -4,7 +4,7 @@ import os
 
 def to_qr(url, rcid):
     img = qrcode.make(url)
-    path = os.getcwd()+'static/Resources/QR/'+str(rcid)+'.png'
+    path ='static/Resources/QR/'+str(rcid)+'.png'
     # how to resolve this
     img.save(path)
     return path
