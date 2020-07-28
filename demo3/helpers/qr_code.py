329bc99ec25dd1c3bdc9dc6ea27fc146cb39761e
乔嘉伟 <qiaojiawei0612@gmail.com>
2020-07-28 00:01:27 -0400

    Added test suite for qr_code helper

--- demo3/helpers/qr_code.py
+++ demo3/helpers/qr_code.py
@@ -1,15 +1,24 @@
 import qrcode
 from PIL import Image
 import os
+import config
+from pathlib import Path
+
+cwd = os.getcwd()
+path = Path(cwd)
+cwd_p = path.parent
+path_p = Path(cwd_p)
+cwd_pp = path_p.parent
 
 def to_qr(url, rcid):
     img = qrcode.make(url)
-    path ='static/Resources/QR/'+str(rcid)+'.png'
+    if config.STATUS == 'TEST':
+        path = str(cwd_pp) + '/static/Resources/QR/'+str(rcid)+'.png'
+    else:
+        path ='static/Resources/QR/'+str(rcid)+'.png'
     # how to resolve this
     img.save(path)
     return path
-
-
 
 
 
---
+++ demo3/test/testQR/testQrCode.py
@@ -0,0 +1,24 @@
+import unittest
+from helpers.qr_code import *
+import os
+from app import cwd
+
+cwd = os.getcwd()
+path = Path(cwd)
+cwd_p = path.parent
+path_p = Path(cwd_p)
+cwd_pp = path_p.parent
+
+
+class testQrCode(unittest.TestCase):
+    """
+    Test the to_qr function in qr_code helper
+    Can only check path
+    """
+    def test_path_normal_url_to_img(self):
+        path = to_qr("iamurl",10)
+        target = str(cwd_pp) + '/static/Resources/QR/'+str(10)+'.png'
+        self.assertEqual(path, target)
+
+if __name__ == "__main__":
+    unittest.main()
