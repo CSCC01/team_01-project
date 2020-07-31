abda27d5f4d30130b83f883fb10f9815de873cee
nessa <vanessa242pierre@gmail.com>
2020-07-31 16:27:52 -0400

    T122: Designed a UI for 'vistit x times between dates' type achievement

--- demo3/static/achievement.js
+++ demo3/static/achievement.js
@@ -3,13 +3,32 @@ document.getElementById("type").addEventListener('change', function (e) {
     document.getElementById("type0").style.display = "flex";
     document.getElementById("type1").style.display = "none";
     document.getElementById("type2").style.display = "none";
+    document.getElementById("type3").style.display = "none";
   } if (e.target.value === "1") {
     document.getElementById("type0").style.display = "none";
     document.getElementById("type1").style.display = "flex";
     document.getElementById("type2").style.display = "none";
+    document.getElementById("type3").style.display = "none";
   } if (e.target.value === "2") {
     document.getElementById("type0").style.display = "none";
     document.getElementById("type1").style.display = "none";
     document.getElementById("type2").style.display = "flex";
+    document.getElementById("type3").style.display = "none";
+  } if (e.target.value === "3") {
+    document.getElementById("type0").style.display = "none";
+    document.getElementById("type1").style.display = "none";
+    document.getElementById("type2").style.display = "none";
+    document.getElementById("type3").style.display = "flex";
+    document.getElementById("type3").style.flexDirection = "column";
   }
 });
+
+function checkbox(dates) {
+    var cb = document.getElementsByName("indefinite");
+    var visability = "flex";
+      if(cb[0].checked){
+       visability = "none";
+      }
+
+    document.getElementById(dates).style.display = visability;
+}
