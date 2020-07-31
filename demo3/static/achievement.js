18c647795e4e69d62f4505aea312fafe7f215722
nessa <vanessa242pierre@gmail.com>
2020-07-31 14:13:49 -0400

    T121: Designed a UI for 'visit in a group of x' type achievement

--- demo3/static/achievement.js
+++ demo3/static/achievement.js
@@ -2,8 +2,14 @@ document.getElementById("type").addEventListener('change', function (e) {
   if (e.target.value === "0") {
     document.getElementById("type0").style.display = "flex";
     document.getElementById("type1").style.display = "none";
+    document.getElementById("type2").style.display = "none";
   } if (e.target.value === "1") {
     document.getElementById("type0").style.display = "none";
     document.getElementById("type1").style.display = "flex";
+    document.getElementById("type2").style.display = "none";
+  } if (e.target.value === "2") {
+    document.getElementById("type0").style.display = "none";
+    document.getElementById("type1").style.display = "none";
+    document.getElementById("type2").style.display = "flex";
   }
 });
