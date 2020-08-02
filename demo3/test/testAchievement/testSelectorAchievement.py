8ac315d6632bbcb169ef3a7c1eb53c9e86a6f794
nessa <vanessa242pierre@gmail.com>
2020-08-01 21:37:44 -0400

    T125: Added test cases for the selector test suite

--- demo3/test/testAchievement/testSelectorAchievement.py
+++ demo3/test/testAchievement/testSelectorAchievement.py
@@ -26,27 +26,46 @@ class SelectAchievementTest(unittest.TestCase):
 
     def test_get_values(self):
         """Tests get_achievement_values() for all achievement types."""
-        achievement1 = Achievements(rid=12, name="test", points=10, experience=15, type=0, value="Item;5")
-        achievement2 = Achievements(rid=13, name="test 2", points=15, experience=20, type=1, value=";6.99")
+        achievement1 = Achievements(rid=12, name="test", points=10, experience=15, type=0, value="Item;5;;;")
+        achievement2 = Achievements(rid=13, name="test 2", points=15, experience=20, type=1, value=";6.99;;;")
+        achievement3 = Achievements(rid=13, name="test 3", points=15, experience=20, type=2, value=";6;;;")
+        achievement4 = Achievements(rid=13, name="test 3", points=15, experience=20, type=2, value=";6;True;;")
+        achievement4 = Achievements(rid=13, name="test 3", points=15, experience=20, type=2, value=";6;False;2020-08-1;2020-08-31")
 
-        self.assertEqual(achievementhelper.get_achievement_data(achievement1), ["Item", "5"])
-        self.assertEqual(achievementhelper.get_achievement_data(achievement2), ["", "6.99"])
+        self.assertEqual(achievementhelper.get_achievement_data(achievement1), ["Item", "5", "", "", ""])
+        self.assertEqual(achievementhelper.get_achievement_data(achievement2), ["", "6.99", "", "", ""])
+        self.assertEqual(achievementhelper.get_achievement_data(achievement2), ["", "6", "", "", ""])
+        self.assertEqual(achievementhelper.get_achievement_data(achievement2), ["", "6", "True", "2020-08-1", "2020-08-31"])
+
 
     def test_get_description(self):
         """Tests get_achievement_description() for all achievement types."""
-        achievement1 = Achievements(rid=12, name="test", points=10, experience=15, type=0, value="Item;5")
-        achievement2 = Achievements(rid=13, name="test 2", points=15, experience=20, type=1, value=";6.99")
+        achievement1 = Achievements(rid=12, name="test", points=10, experience=15, type=0, value="Item;5;;;")
+        achievement2 = Achievements(rid=13, name="test 2", points=15, experience=20, type=1, value=";6.99;;;")
+        achievement3 = Achievements(rid=13, name="test 3", points=15, experience=20, type=2, value=";2;;;")
+        achievement4 = Achievements(rid=13, name="test 4", points=15, experience=20, type=3, value=";5;True;;")
+        achievement5 = Achievements(rid=13, name="test 5", points=15, experience=20, type=3, value=";5;False;2020-08-01;2020-08-31")
 
         self.assertEqual(achievementhelper.get_achievement_description(achievement1), "Buy Item 5 times.")
         self.assertEqual(achievementhelper.get_achievement_description(achievement2), "Spend $6.99 in a single visit.")
+        self.assertEqual(achievementhelper.get_achievement_description(achievement3), "Visit with a group of at least 2.")
+        self.assertEqual(achievementhelper.get_achievement_description(achievement4), "Visit 5 times.")
+        self.assertEqual(achievementhelper.get_achievement_description(achievement5), "Visit 5 times between 2020-08-01 and 2020-08-31.")
+
 
     def test_get_progress_max(self):
         """Tests get_achievement_progress_maximum() for all achievement types."""
-        achievement1 = Achievements(rid=12, name="test", points=10, experience=15, type=0, value="Item;5")
-        achievement2 = Achievements(rid=13, name="test 2", points=15, experience=20, type=1, value=";6.99")
+        achievement1 = Achievements(rid=12, name="test", points=10, experience=15, type=0, value="Item;5;;;")
+        achievement2 = Achievements(rid=13, name="test 2", points=15, experience=20, type=1, value=";6.99;;;")
+        achievement3 = Achievements(rid=13, name="test 3", points=15, experience=20, type=2, value=";6;;;")
+        achievement4 = Achievements(rid=13, name="test 3", points=15, experience=20, type=2, value=";6;True;;")
+        achievement4 = Achievements(rid=13, name="test 3", points=15, experience=20, type=2, value=";6;False;2020-08-1;2020-08-31")
 
         self.assertEqual(achievementhelper.get_achievement_progress_maximum(achievement1), 5)
         self.assertEqual(achievementhelper.get_achievement_progress_maximum(achievement2), 1)
+        self.assertEqual(achievementhelper.get_achievement_progress_maximum(achievement3), 6)
+        self.assertEqual(achievementhelper.get_achievement_progress_maximum(achievement4), 6)
+        self.assertEqual(achievementhelper.get_achievement_progress_maximum(achievement5), 6)
 
 
     def test_get_nonexistent_achievements(self):
