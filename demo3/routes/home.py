99f6aeaf7cc1dcb5bb6ddc1ee9c81e2f4160df60
yh-su <yolanda.su@mail.utoronto.ca>
2020-08-06 01:01:16 -0400

    set up new owner home page UI, waiting on S028 to complete

--- demo3/routes/home.py
+++ demo3/routes/home.py
@@ -5,9 +5,13 @@
 ###################################################
 
 from flask import Flask, render_template, request, redirect, url_for, session, Blueprint
+from databaseHelpers.redeemedCoupons import *
+from databaseHelpers.achievement import *
+from databaseHelpers.achievementProgress import *
 from databaseHelpers.restaurant import *
 from databaseHelpers.employee import *
 from databaseHelpers.user import *
+from datetime import datetime
 
 home_page = Blueprint('home_page', __name__, template_folder='templates')
 
@@ -27,6 +31,35 @@ def home():
         rname = get_restaurant_name_by_rid(rid)
         raddress = get_restaurant_address(rid)
         return render_template('home.html', rname = rname, raddress = raddress)
+
+    # Owner view of home page
+    elif session['type'] == 1:
+        rid = get_rid(session["account"])
+        rname = get_restaurant_name_by_rid(rid)
+        raddress = get_restaurant_address(rid)
+        coupons = get_redeemed_coupons_by_rid(rid)
+
+        # get 3 top owned coupons
+        top_owned_coupons = coupons.copy()
+        top_owned_coupons.sort(key=lambda x: x.get('holders'))
+        top_owned_coupons = top_owned_coupons[-3:]
+        top_owned_coupons.reverse()
+        
+        # get 3 top used coupons
+        top_used_coupons = coupons.copy()
+        top_used_coupons.sort(key=lambda x: x.get('used'))
+        top_used_coupons = top_used_coupons[-3:]
+        top_used_coupons.reverse()
+
+        # get the 3 achievements started by the most customers
+        achievements = get_achievements_with_progress_entry_count(get_achievements_by_rid(rid))
+        achievements.sort(key=lambda x: x.get('progress_entries'))
+        achievements = achievements[-3:]
+        achievements.reverse()
+
+        return render_template('home.html', rname = rname, raddress = raddress, today = date.today(),
+                                top_owned_coupons = top_owned_coupons, top_used_coupons = top_used_coupons,
+                                achievements = achievements)
 
     # default view of home page
     else:
