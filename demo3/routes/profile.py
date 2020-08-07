54977d288c6ea6933dc7c49f75d50277acc0a0cb
Yolanda Su <yolanda.su@mail.utoronto.ca>
2020-08-07 16:40:04 -0400

    S030: Allow owners to edit the name and address of their restaurant via the profile page. (#41)
    * added backend function for updating restaurant name and address
    
    * added new page for editing restaurant info and linked to profile page
    
    * added test suite for update_restaurant_information()
    
    * fixed copy-paste error in error message on editRestaurantInfo.html
    
    * changed every if len(errmsg) == 0 to if not errmsg for code style consistency

--- demo3/routes/profile.py
+++ demo3/routes/profile.py
@@ -8,6 +8,8 @@ from flask import Flask, render_template, request, redirect, url_for, session, B
 profile_page = Blueprint('profile_page', __name__, template_folder='templates')
 from databaseHelpers.user import *
 
+from databaseHelpers.restaurant import *
+
 @profile_page.route('/profile.html')
 @profile_page.route('/profile')
 def profile():
@@ -16,4 +18,40 @@ def profile():
         return redirect(url_for('login_page.login'))
     else:
         user = get_user(session['account'])
+        # if user is a restaurant owner
+        if session['account'] == 1:
+            rid = get_rid(session['account'])
+            rname = get_restaurant_name_by_rid(rid)
+            raddress = get_restaurant_address(rid)
+
+            return render_template('profile.html', rname = rname, raddress = raddress, user = user)
+
         return render_template('profile.html', user = user)
+
+@profile_page.route('/editRestaurantInfo.html', methods=['GET', 'POST'])
+@profile_page.route('/editRestaurantInfo', methods=['GET', 'POST'])
+def edit_restaurant_info():
+    # If someone is not logged in redirects them to login page
+    if 'account' not in session:
+        return redirect(url_for('login_page.login'))
+    
+    # if user is a restaurant owner
+    if session['type'] == 1:
+        rid = get_rid(session['account'])
+        rname = get_restaurant_name_by_rid(rid)
+        raddress = get_restaurant_address(rid)
+
+        if request.method == 'POST':
+            rname = request.form['rname']
+            raddress = request.form['address']
+            restaurant = get_resturant_by_rid(rid)
+
+            errmsg = update_restaurant_information(restaurant, rname, raddress)
+            
+            if not errmsg:
+                return redirect(url_for('profile_page.profile'))
+            return render_template('editRestaurantInfo.html', rname = rname, raddress = raddress, errmsg = errmsg)
+
+        return render_template('editRestaurantInfo.html', rname = rname, raddress = raddress)
+    else:
+        return redirect(url_for('home_page.home'))
