d5dcf0e035a618619c282edb4fa6983a07d578d7
mifafaovo <jiawei.qiao@mail.utoronto.ca>
2020-08-09 17:46:23 -0400

    Fixed docs in redeemedCoupons.py

--- demo3/databaseHelpers/redeemedCoupons.py
+++ demo3/databaseHelpers/redeemedCoupons.py
@@ -10,6 +10,17 @@ else:
 
 
 def get_redeemed_coupons_by_rid(rid):
+    """
+    Add two keys in coupons dictionary which represents the holders and used coupons.
+
+    Args:
+        rid: The restaurant ID that corresponds to the Restaurant that is fetched.
+
+    Returns:
+        a list of coupon which has two extra key, 'holders' and 'used'
+        'holders' records the number of users who currently own this coupon
+        'used' records the number of previous usage of this coupon
+    """
     coupons = get_coupons(rid)
 
     for c in coupons:
@@ -22,12 +33,33 @@ def get_redeemed_coupons_by_rid(rid):
 
 
 def mark_redeem_coupon_used_by_rcid(rcid):
+    """
+    Change valid to one by the given rcid
+
+    Args:
+        rcid: The redeemed_coupon ID that corresponds to the Redeemed_Coupon that is fetched.
+
+    Returns:
+        the coupon being changed
+    """
     coupon = Redeemed_Coupons.query.filter(Redeemed_Coupons.rcid == rcid).first()
     coupon.valid = 0
     db.session.commit()
     return coupon
 
+
 def find_rcid_by_cid_and_uid(cid, uid):
+    """
+    Get the rcid by the given cid and uid
+
+    Args:
+        cid: The coupon ID that corresponds to the Coupon that is fetched.
+        uid: The user ID that corresponds to the User that is fetched.
+
+    Returns:
+        (if found) redeemed_coupon ID that corresponds to the Redeemed_Coupon that is fetched.
+        (if not) 'Not Found'
+    """
     coupon = Redeemed_Coupons.query.filter(Redeemed_Coupons.cid == cid,
                                            Redeemed_Coupons.uid == uid, Redeemed_Coupons.valid == 1).first()
     if coupon:
@@ -35,14 +67,21 @@ def find_rcid_by_cid_and_uid(cid, uid):
     return "Not Found"
 
 
-
 def get_redeemed_coupons_by_uid(uid):
+    """
+    Get a list of the redeemed coupons by uid
+
+    Args:
+        uid: The user ID that corresponds to the User that is fetched.
+
+    Returns:
+        a list of the redeemed coupons with extra fields restaurant name
+    """
     coupons = Redeemed_Coupons.query.filter(Redeemed_Coupons.uid == uid, Redeemed_Coupons.valid == 1).all()
     coupon_list = []
 
     for c in coupons:
         dict = get_coupon_by_cid(c.cid)
-
         dict["rname"] = get_restaurant_name_by_rid(c.rid)
         dict["raddress"] = get_restaurant_address(c.rid)
         coupon_list.append(dict)
@@ -51,6 +90,17 @@ def get_redeemed_coupons_by_uid(uid):
 
 
 def insert_redeemed_coupon(cid, uid, rid):
+    """
+    Insert a redeemed coupon to database
+
+    Args:
+        cid: The coupon ID that corresponds to the Coupon that is fetched.
+        uid: The user ID that corresponds to the User that is fetched.
+        rid: The restaurant ID that corresponds to the Restaurant that is fetched.
+
+    Returns:
+        the rcid of the inserted coupon
+    """
     coupon = Redeemed_Coupons(cid = cid, uid = uid, rid = rid, valid = 1)
     db.session.add(coupon)
     db.session.commit()
