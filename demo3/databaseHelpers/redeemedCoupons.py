51e938419a6d36e2ec0448f861725210c4fec1d7
Jiawei Qiao <61985646+mifafaovo@users.noreply.github.com>
2020-08-10 15:46:31 -0400

    F018/F019: Not showing achievement in progress/redeemed coupon which has been expired more than 6 months (#52)
    * Fixed will delete achievement in progress/redeemed coupon expired 6+ months
    
    * Fixed NoneType Error when coupon has no expire date

--- demo3/databaseHelpers/redeemedCoupons.py
+++ demo3/databaseHelpers/redeemedCoupons.py
@@ -1,6 +1,8 @@
 from models import Coupon, Redeemed_Coupons, User
 from databaseHelpers.coupon import *
 from databaseHelpers.restaurant import *
+from datetime import date
+from dateutil.relativedelta import relativedelta
 
 import config
 if config.STATUS == "TEST":
@@ -82,9 +84,10 @@ def get_redeemed_coupons_by_uid(uid):
 
     for c in coupons:
         dict = get_coupon_by_cid(c.cid)
-        dict["rname"] = get_restaurant_name_by_rid(c.rid)
-        dict["raddress"] = get_restaurant_address(c.rid)
-        coupon_list.append(dict)
+        if not dict["expiration"] or dict["expiration"] + relativedelta(months=+6) > date.today():
+            dict["rname"] = get_restaurant_name_by_rid(c.rid)
+            dict["raddress"] = get_restaurant_address(c.rid)
+            coupon_list.append(dict)
 
     return coupon_list
 
